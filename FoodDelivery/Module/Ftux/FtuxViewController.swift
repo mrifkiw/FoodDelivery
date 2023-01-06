acc5b15063f50ec8630726fc0cd9a263c11f0279
Muhammad Rifki Widadi <110385113+rifki-pegipegi@users.noreply.github.com>
2023-01-06 16:03:11 +0700

    feat(launchscreen): add style new launchscreen

---
+++ FoodDelivery/Module/Ftux/FtuxViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FtuxViewController.swift
+//  FoodDelivery
+//
+//  Created by Muhammad Rifki Widadi on 06/01/23.
+//
+
+import UIKit
+
+class FtuxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
